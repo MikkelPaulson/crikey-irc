e6743fae1133759a139c28cae6a6d562d4d10231
Mikkel Paulson <git@email.mikkel.ca>
2020-08-08 17:03:02 -0400

    impl basic connection r/w

--- src/connection.rs
+++ src/connection.rs
@@ -1,45 +1,38 @@
 use std::io;
 use std::io::prelude::*;
 use std::net;
-//use std::sync::mpsc;
 
 pub struct Connection<'a> {
     reader: Box<dyn 'a + io::BufRead>,
-    //writer: Box<dyn 'a + Write>,
+    writer: Box<dyn 'a + Write>,
 }
 
 impl Connection<'_> {
     pub fn new(stream: &net::TcpStream) -> Connection {
-        //stream.set_nonblocking(true).unwrap();
+        stream.set_nonblocking(true).unwrap();
 
         let reader = io::BufReader::new(stream);
-        //let writer = io::BufWriter::new(stream);
 
         Connection {
             reader: Box::new(reader),
-            //writer: Box::new(writer),
+            writer: Box::new(stream),
         }
     }
 
     pub fn poll(&mut self) -> Option<String> {
         let mut buffer = String::new();
-        if let Ok(_) = self.reader.read_line(&mut buffer) {
-            Some(buffer)
-        } else {
-            None
-        }
 
-        //match self.reader.read_line(&mut buffer) {
-        //Ok(_) => Some(buffer),
-        //Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => None,
-        //Err(e) => panic!("IO error: {}", e)
-        //}
+        match self.reader.read_line(&mut buffer) {
+            Ok(_) => Some(buffer),
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => None,
+            Err(e) => panic!("IO error: {}", e),
+        }
     }
 
     //pub fn sendCommand(command: Command);
 
-    //pub fn send_command_raw(&mut self, command: String) -> std::io::Result<()> {
-    //self.writer.write(command.as_bytes())?;
-    //Ok(())
-    //}
+    pub fn send_command_raw(&mut self, command: String) -> std::io::Result<()> {
+        self.writer.write(command.as_bytes())?;
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-//use std::env;
 use std::io;
-use std::io::prelude::*;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
@@ -8,14 +6,11 @@ use std::time::Duration;
 mod connection;
 
 fn main() -> io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:6667").expect("Could not connect to server.");
-
-    stream.write(b"NICK foo\r\n")?;
-    stream.write(b"USER pjohnson local remote :Potato Johnson\r\n")?;
+    let stream = TcpStream::connect("127.0.0.1:6667").expect("Could not connect to server.");
 
     let mut connection = connection::Connection::new(&stream);
-    //connection.send_command_raw("NICK foo\r\n".to_string())?;
-    //connection.send_command_raw("USER pjohnson local remote :Potato Johnson\r\n".to_string())?;
+    connection.send_command_raw("NICK foo\r\n".to_string())?;
+    connection.send_command_raw("USER pjohnson local remote :Potato Johnson\r\n".to_string())?;
 
     // hangs on close!
     loop {
