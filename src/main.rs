08d41114f6072291a5f724af941c447f1b4e30db
Mikkel Paulson <git@email.mikkel.ca>
2020-08-16 15:56:23 -0400

    use Rc/RefCell for Connection as well

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::connection::Connect;
 use std::cell::RefCell;
 use std::io;
 use std::net::TcpStream;
@@ -14,29 +15,36 @@ fn main() -> io::Result<()> {
 
     let dispatcher = Rc::new(RefCell::new(dispatcher::Dispatcher::new()));
 
-    let mut connection = connection::Connection::new(&stream, dispatcher.clone());
+    let connection = Rc::new(RefCell::new(connection::Connection::new(
+        &stream,
+        dispatcher.clone(),
+    )));
 
     let terminal = terminal::Terminal::new(io::stdin());
 
-    connection.send_command(connection::Command::Nick {
-        nickname: "foo".to_string(),
-        hopcount: None,
-    })?;
-    connection.send_command(connection::Command::User {
-        username: "pjohnson".to_string(),
-        hostname: "local".to_string(),
-        servername: "remote".to_string(),
-        realname: "Potato Johnson".to_string(),
-    })?;
+    connection
+        .borrow_mut()
+        .send_command(connection::Command::Nick {
+            nickname: "foo".to_string(),
+            hopcount: None,
+        })?;
+    connection
+        .borrow_mut()
+        .send_command(connection::Command::User {
+            username: "pjohnson".to_string(),
+            hostname: "local".to_string(),
+            servername: "remote".to_string(),
+            realname: "Potato Johnson".to_string(),
+        })?;
 
     loop {
-        if connection.poll() {
+        if connection.borrow_mut().poll() {
             continue;
         }
 
         if let Some(mut input) = terminal.read() {
             input.pop(); // trim trailing newline
-            connection.send_command_raw(input)?;
+            connection.borrow_mut().send_command_raw(input)?;
         }
 
         thread::sleep(Duration::from_millis(100));
