70979bcde54f4aff58e06b5cce3523e7f8c62eb0
Mikkel Paulson <git@email.mikkel.ca>
2020-08-22 17:09:48 -0400

    accept server IP/port as first command-line argument

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use irustc_bot::run;
+use std::env;
 use std::io;
 
 fn main() -> io::Result<()> {
-    run("irc-server:6667")
+    let server_addr = env::args().nth(1).unwrap_or("127.0.0.1:6667".to_string());
+
+    run(server_addr)
 }
