6ed26defd9da1abd47e89c3390364da21d358b59
Mikkel Paulson <git@email.mikkel.ca>
2020-08-28 11:27:38 -0400

    Add command component: (Target|Server|Host)Mask
    A mask used to reference multiple servers or hosts.
    
    The goal is to standardize message parsing by using enums and structs to
    validate and represent protocol-level types. These types are not yet
    wired up; the immediate goal is to implement the syntax requirements of
    RFC 2812.
    
    https://tools.ietf.org/html/rfc2812#section-2.3.1

--- src/connection/types/mod.rs
+++ src/connection/types/mod.rs
@@ -1,5 +1,6 @@
 pub use self::key::Key;
 pub use self::nickname::Nickname;
+pub use self::target_mask::{HostMask, ServerMask, TargetMask};
 pub use self::user::User;
 
 pub use self::errors::ParseError;
@@ -7,4 +8,5 @@ pub use self::errors::ParseError;
 mod errors;
 mod key;
 mod nickname;
+mod target_mask;
 mod user;
