28de5ee41c342653243d12945b6ca97d5b740f97
Mikkel Paulson <git@email.mikkel.ca>
2020-08-28 11:27:38 -0400

    Add command component: Channel.*
    A channel is a named group of one or more users which will all receive
    messages addressed to that channel.
    
    The goal is to standardize message parsing by using enums and structs to
    validate and represent protocol-level types. These types are not yet
    wired up; the immediate goal is to implement the syntax requirements of
    RFC 2812.
    
    https://tools.ietf.org/html/rfc2812#section-2.3.1

--- src/connection/types/mod.rs
+++ src/connection/types/mod.rs
@@ -1,3 +1,4 @@
+pub use self::channel::{Channel, ChannelID, ChannelName, ChannelType};
 pub use self::host::{Host, Hostname, IpAddr, Ipv4Addr, Ipv6Addr, Servername};
 pub use self::key::Key;
 pub use self::nickname::Nickname;
@@ -6,6 +7,7 @@ pub use self::user::User;
 
 pub use self::errors::ParseError;
 
+mod channel;
 mod errors;
 mod host;
 mod key;
