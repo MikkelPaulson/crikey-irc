806c82f1e1a73f59ab5c4f8749ff5fd62b42a14d
Mikkel Paulson <git@email.mikkel.ca>
2020-08-28 11:27:38 -0400

    Add command component: Host(name)?|Servername
    A reference by hostname or IP address to a particular host or server.
    
    The goal is to standardize message parsing by using enums and structs to
    validate and represent protocol-level types. These types are not yet
    wired up; the immediate goal is to implement the syntax requirements of
    RFC 2812.
    
    https://tools.ietf.org/html/rfc2812#section-2.3.1

--- src/connection/types/mod.rs
+++ src/connection/types/mod.rs
@@ -1,3 +1,4 @@
+pub use self::host::{Host, Hostname, IpAddr, Ipv4Addr, Ipv6Addr, Servername};
 pub use self::key::Key;
 pub use self::nickname::Nickname;
 pub use self::target_mask::{HostMask, ServerMask, TargetMask};
@@ -6,6 +7,7 @@ pub use self::user::User;
 pub use self::errors::ParseError;
 
 mod errors;
+mod host;
 mod key;
 mod nickname;
 mod target_mask;
