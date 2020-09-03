2a41b0a728ba9ab0c0946f543196ee1023978e3d
Mikkel Paulson <git@email.mikkel.ca>
2020-09-03 00:04:39 -0400

    Integrate ParseError into types mod
    Still not sure where this should live, but seems like as good a place as
    any.

--- src/connection/types/mod.rs
+++ src/connection/types/mod.rs
@@ -1,5 +1,4 @@
 pub use self::channel::{Channel, ChannelID, ChannelName, ChannelType};
-pub use self::errors::ParseError;
 pub use self::host::{Host, Hostname, IpAddr, Ipv4Addr, Ipv6Addr, Servername};
 pub use self::key::Key;
 pub use self::keyword_list::KeywordList;
@@ -10,7 +9,6 @@ pub use self::target_mask::{HostMask, ServerMask, TargetMask};
 pub use self::user::User;
 
 mod channel;
-mod errors;
 mod host;
 mod key;
 mod keyword_list;
@@ -19,3 +17,27 @@ mod nickname;
 mod stats_query;
 mod target_mask;
 mod user;
+
+use std::error::Error;
+use std::fmt;
+
+#[derive(PartialEq, Debug)]
+pub struct ParseError(&'static str);
+
+impl ParseError {
+    pub fn new(struct_name: &'static str) -> Self {
+        ParseError(struct_name)
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Unable to parse component: {}", self)
+    }
+}
+
+impl Error for ParseError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
