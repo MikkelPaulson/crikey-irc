abb9644b12aea1d4912358508cbac17097dfba5c
Mikkel Paulson <git@email.mikkel.ca>
2020-09-03 16:15:52 -0400

    clean server tests
    Update server tests (Host, Hostname, Servername) to follow newer format
    used by other entities.

--- src/connection/entity/server.rs
+++ src/connection/entity/server.rs
@@ -33,6 +33,56 @@ impl From<Host> for String {
     }
 }
 
+#[cfg(test)]
+mod test_host {
+    use super::*;
+
+    #[test]
+    fn invalid() {
+        assert!("abc.d\nf.ghi".parse::<Host>().is_err());
+        assert!("abc.d🥔️f.ghi".parse::<Host>().is_err());
+        assert!("abc.d f.ghi".parse::<Host>().is_err());
+        assert!("abc.-ef.ghi".parse::<Host>().is_err());
+        assert!("abc.de-.ghi".parse::<Host>().is_err());
+        assert!("".parse::<Host>().is_err());
+        assert!("abc..ghi".parse::<Host>().is_err());
+        assert!(".a".parse::<Host>().is_err());
+        assert!("abc.def.ghi.".parse::<Host>().is_err());
+    }
+
+    #[test]
+    fn valid() {
+        assert_eq!(
+            Ok(Host::Hostname(Hostname("abcdefghi".to_string()))),
+            "abcdefghi".parse::<Host>()
+        );
+        assert_eq!(
+            Ok(Host::Hostname(Hostname("abc.d-f.ghi".to_string()))),
+            "abc.d-f.ghi".parse::<Host>()
+        );
+        assert_eq!(
+            Ok(Host::Hostaddr("1.2.3.4".parse().unwrap())),
+            "1.2.3.4".parse::<Host>()
+        );
+        assert_eq!(
+            Ok(Host::Hostaddr("::1".parse().unwrap())),
+            "::1".parse::<Host>()
+        );
+    }
+
+    #[test]
+    fn into_string() {
+        assert_eq!(
+            "1.2.3.4".to_string(),
+            String::from(Host::Hostaddr("1.2.3.4".parse().unwrap()))
+        );
+        assert_eq!(
+            "abc.def.ghi".to_string(),
+            String::from(Host::Hostname(Hostname("abc.def.ghi".to_string())))
+        );
+    }
+}
+
 #[derive(PartialEq, Debug)]
 pub struct Servername(String);
 
@@ -86,103 +136,36 @@ fn name_from_string(raw: &str) -> Option<String> {
 }
 
 #[cfg(test)]
-mod tests {
+mod test_hostname_servername {
     use super::*;
 
     #[test]
-    fn invalid_chars() {
-        assert!("abc.d\nf.ghi".parse::<Host>().is_err());
-        assert!("abc.d🥔️f.ghi".parse::<Host>().is_err());
-        assert!("abc.d f.ghi".parse::<Host>().is_err());
-        assert!("abc.-ef.ghi".parse::<Host>().is_err());
-        assert!("abc.de-.ghi".parse::<Host>().is_err());
-        assert!("".parse::<Host>().is_err());
-    }
-
-    #[test]
-    fn invalid_dot_placement() {
-        assert!("abc..ghi".parse::<Host>().is_err());
-        assert!(".a".parse::<Host>().is_err());
-
-        // All non-relative hostnames technically terminate with a trailing dot,
-        // but the RFC disagrees.
-        assert!("abc.def.ghi.".parse::<Host>().is_err());
+    fn invalid() {
+        assert!("abc.d\nf.ghi".parse::<Hostname>().is_err());
+        assert!("abc.d🥔️f.ghi".parse::<Servername>().is_err());
+        assert!("abc.d f.ghi".parse::<Hostname>().is_err());
+        assert!("abc.-ef.ghi".parse::<Servername>().is_err());
+        assert!("abc.de-.ghi".parse::<Hostname>().is_err());
+        assert!("".parse::<Servername>().is_err());
+        assert!("abc..ghi".parse::<Hostname>().is_err());
+        assert!(".a".parse::<Servername>().is_err());
+        assert!("abc.def.ghi.".parse::<Hostname>().is_err());
     }
 
     #[test]
     fn valid() {
         assert_eq!(
-            Host::Hostname(Hostname("abcdefghi".to_string())),
-            "abcdefghi"
-                .parse::<Host>()
-                .expect("Hostname without dots should be valid")
+            Ok(Servername("abc.def.ghi".to_string())),
+            "abc.def.ghi".parse::<Servername>()
         );
         assert_eq!(
-            Host::Hostname(Hostname("abc.d-f.ghi".to_string())),
-            "abc.d-f.ghi"
-                .parse::<Host>()
-                .expect("Dotted hostname should be valid")
-        );
-    }
-
-    #[test]
-    fn ipv4() {
-        assert_eq!(
-            Host::Hostaddr("1.2.3.4".parse().unwrap()),
-            "1.2.3.4"
-                .parse::<Host>()
-                .expect("Expect host mask to accept an IPv4 address")
-        )
-    }
-
-    #[test]
-    fn ipv6() {
-        assert_eq!(
-            Host::Hostaddr("::1".parse().unwrap()),
-            "::1"
-                .parse::<Host>()
-                .expect("Expect host mask to accept an IPv6 address")
-        );
-        assert_eq!(
-            Host::Hostaddr("2001:db8::ff00:42:8329".parse().unwrap()),
-            "2001:db8::ff00:42:8329"
-                .parse::<Host>()
-                .expect("Expect host mask to accept an IPv6 address")
-        );
-        assert_eq!(
-            Host::Hostaddr("2001:0db8:0000:0000:0000:ff00:0042:8329".parse().unwrap()),
-            "2001:0db8:0000:0000:0000:ff00:0042:8329"
-                .parse::<Host>()
-                .expect("Expect host mask to accept an IPv6 address")
-        );
-    }
-
-    #[test]
-    fn valid_server_and_host_name() {
-        assert_eq!(
-            Servername("abc.def.ghi".to_string()),
-            "abc.def.ghi"
-                .parse::<Servername>()
-                .expect("Failed to accept a valid server name")
-        );
-        assert_eq!(
-            Hostname("abc.def.ghi".to_string()),
-            "abc.def.ghi"
-                .parse::<Hostname>()
-                .expect("Failed to accept a valid hostname")
+            Ok(Hostname("abc.def.ghi".to_string())),
+            "abc.def.ghi".parse::<Hostname>()
         );
     }
 
     #[test]
     fn into_string() {
-        assert_eq!(
-            "1.2.3.4".to_string(),
-            String::from(Host::Hostaddr("1.2.3.4".parse::<IpAddr>().unwrap()))
-        );
-        assert_eq!(
-            "abc.def.ghi".to_string(),
-            String::from(Host::Hostname(Hostname("abc.def.ghi".to_string())))
-        );
         assert_eq!(
             "abc.def.ghi".to_string(),
             String::from(Hostname("abc.def.ghi".to_string()))
