35fd04f3c382d73ae9a9d18d6ca63fdd484febea
andy5995 <andy400-dev@yahoo.com>
2020-09-06 23:17:21 -0500

    implement config file

---
+++ src/config.rs
@@ -0,0 +1,49 @@
+use configster;
+
+pub struct Data {
+    pub realname: String,
+    pub nick: String,
+    pub password: String,
+    pub server_addr: String,
+    pub server_port: u32,
+}
+
+impl Data {
+    pub fn new() -> Self {
+        Self {
+            realname: String::new(),
+            nick: String::new(),
+            password: String::new(),
+            server_addr: String::new(),
+            server_port: 0,
+        }
+    }
+}
+
+pub fn get() -> Data {
+    let config_vec = configster::parse_file("./crikeyrc", ',').expect("Error reading config file");
+    let mut config_data = Data::new();
+    for i in &config_vec {
+        match i.option.as_ref() {
+            "realname" => config_data.realname = i.value.primary.clone(),
+            "nick" => config_data.nick = i.value.primary.clone(),
+            "password" => config_data.password = i.value.primary.clone(),
+            "server_addr" => {
+                config_data.server_addr = i.value.primary.clone();
+                match i.value.attributes.get(0).is_some() {
+                    true => {
+                        if i.value.attributes[0] == "server_port" {
+                            config_data.server_port =
+                                i.value.attributes[0].parse().expect("Invalid port number")
+                        } else {
+                            println!("Invalid port value");
+                        }
+                    }
+                    false => println!("No port option given for server"),
+                }
+            }
+            _ => println!("Invalid Option"),
+        }
+    }
+    config_data
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use crikey_irc::run;
 use std::env;
 use std::io;
+mod config;
 
 fn main() -> io::Result<()> {
-    let server_addr = env::args().nth(1).unwrap_or("127.0.0.1:6667".to_string());
-    let nick = env::args().nth(2).unwrap_or("spudly".to_string());
+    let config_data = config::get();
+
+    let server_addr = env::args().nth(1).unwrap_or(format!(
+        "{}:{}",
+        config_data.server_addr, config_data.server_port
+    ));
+    let nick = env::args().nth(2).unwrap_or(config_data.nick);
     let username = env::args().nth(3).unwrap_or("pjohnson".to_string());
-    let realname = env::args().nth(4).unwrap_or("Potato Johnson".to_string());
+    let realname = env::args().nth(4).unwrap_or(config_data.realname);
 
     run(server_addr, nick, username, realname)
 }
