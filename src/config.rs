a0b4505bd728a8d6c009db2445ede85c7a1de50f
andy5995 <andy400-dev@yahoo.com>
2020-09-08 00:41:21 -0500

    create a function for getting the config file path

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,19 @@
 use configster;
+use std::env;
+
+pub fn get_filename(homedir: &str) -> String {
+    let config_home: String = match env::var("XDG_CONFIG_HOME") {
+        Ok(val) => val,
+        Err(_e) => format!("{}{}", homedir, "/.config").to_string(),
+    };
+
+    let mut config_file = config_home + "/crikeyrc";
+
+    if !std::path::Path::new(&config_file).exists() {
+        config_file = "./crikeyrc".to_string();
+    }
+    config_file
+}
 
 #[derive(Debug, PartialEq)]
 pub struct Data {
--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,10 @@ fn main() -> io::Result<()> {
         Some(homedir) => homedir.to_str().unwrap().into(),
     };
 
-    let config_home: String = match env::var("XDG_CONFIG_HOME") {
-        Ok(val) => val,
-        Err(_e) => homedir.clone() + "/.config",
-    };
-
-    let mut config_file = config_home + "/crikeyrc";
-
-    if !std::path::Path::new(&config_file).exists() {
-        config_file = "./crikeyrc".to_string();
-    }
-
+    let config_file = config::get_filename(&homedir);
     let mut config_data = config::Data::new();
     config_data.get(&config_file);
+
     let server_addr = env::args().nth(1).unwrap_or(config_data.server_addr);
     let nick = env::args().nth(2).unwrap_or(config_data.nick);
     let username = env::args().nth(3).unwrap_or("pjohnson".to_string());
